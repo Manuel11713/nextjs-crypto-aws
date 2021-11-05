ac3896f0196cef6b9c3816c402ae63015da55961
Manuel Escobedo Lopez <manuelfx117@gmail.com>
2021-11-05 11:26:33 -0600

    add ssr to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,17 @@
-const SSRPage = () => (
+const SSRPage = ({ data }) => (
   <main>
     <h1>SSR Page with Dogs</h1>
+    <p>{data.randomNumber}</p>
   </main>
 );
 export default SSRPage;
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const data = {
+    randomNumber: Math.random(),
+  };
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
